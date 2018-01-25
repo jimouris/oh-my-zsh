5ba75c1e4773bc492148ca86c128c8f462b174fd
Dimitris Mouris <jimouris@gmail.com>
2018-01-25 17:11:39 +0200

    Minor fix

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -170,18 +170,16 @@ alias home='cd ${HOME}'
 alias e="emacs"
 alias top="htop"
 
-# Load command-not-found on Debian-based distributions.
-if [[ -s '/etc/zsh_command_not_found' ]]; then
-  source '/etc/zsh_command_not_found'
-# Load command-not-found on Arch Linux-based distributions.
-elif [[ -s '/usr/share/doc/pkgfile/command-not-found.zsh' ]]; then
-  source '/usr/share/doc/pkgfile/command-not-found.zsh'
-# Return if requirements are not found.
-else
-  return 1
-fi
-
-alias cowfortune='xcowsay “$(fortune -s)”'
+# # Load command-not-found on Debian-based distributions.
+# if [[ -s '/etc/zsh_command_not_found' ]]; then
+#   source '/etc/zsh_command_not_found'
+# # Load command-not-found on Arch Linux-based distributions.
+# elif [[ -s '/usr/share/doc/pkgfile/command-not-found.zsh' ]]; then
+#   source '/usr/share/doc/pkgfile/command-not-found.zsh'
+# # Return if requirements are not found.
+# else
+#   return 1
+# fi
 
 encAES () {
     openssl enc -AES256 -in $1 -out $1.encrypted
